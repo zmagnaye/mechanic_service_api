2ef0a5761e42f5fc84117ef0b6e976e0aa664ffb
Zari <zjcmagnaye@gmail.com>
2025-12-15 22:54:46 -0800

    Fixed DB

--- app/create_tables.py
+++ app/create_tables.py
@@ -1,6 +1,5 @@
-from app import create_app
-from app.extensions import db
-from app.config import ProductionConfig
+from app import create_app, db
+from config import ProductionConfig
 
 app = create_app(ProductionConfig)
 
